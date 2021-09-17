26c3e6c6d8e3fc3e76299b5518b6669dd1ce6007
Maxim Zakharov <maxim.m.zakharov@intel.com>
2021-09-17 23:15:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-#apt-get update
+apt-get update
 apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg git
+
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
+echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/oneAPI.list" -o APT::Get::List-Cleanup="0"
 
 COMPONENTS="intel-oneapi-compiler-dpcpp-cpp"
 apt-get install -y $COMPONENTS

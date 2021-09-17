4673cf50655b3105f47146c3196923431b87420f
Maxim Zakharov <maxim.m.zakharov@intel.com>
2021-09-17 23:01:05 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y wget build-essential pkg-config cmake ca-certificates gnupg git
+
+COMPONENTS="intel-oneapi-compiler-dpcpp-cpp"
+sudo apt-get install -y $COMPONENTS
+
+icpc hello.cpp
